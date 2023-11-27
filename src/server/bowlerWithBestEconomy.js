b3f058e3e7504ca348ac4b38ec2395d7eab94eb4
baskar <baskar15082000@gmail.com>
2023-11-27 16:37:11 +0530

    problem 9

---
+++ src/server/bowlerWithBestEconomy.js
@@ -0,0 +1,67 @@
+//9.Find the bowler with the best economy in super overs
+function findRunsAndBallsInSuperOver(deliveries) {
+  const info = [];
+  deliveries.reduce((acc, delivery) => {
+    if (delivery.is_super_over !== "0") {
+      const bowler = delivery.bowler;
+      if (delivery.wide_runs === "0" && delivery.noball_runs === "0") {
+        if (!info[bowler]) {
+          info[bowler] = {
+            runs: 0,
+            balls: 0,
+          };
+        }
+        info[bowler].runs += parseInt(delivery.total_runs);
+        info[bowler].balls++;
+      } else {
+        if (!info[bowler]) {
+          info[bowler] = {
+            runs: 0,
+            balls: 0,
+          };
+        }
+        info[bowler].runs += parseInt(delivery.total_runs);
+      }
+    }
+
+    return acc;
+  }, {});
+
+  return info;
+}
+
+function toCalculateEconomy(runsAndBalls) {
+  Object.keys(runsAndBalls).forEach((obj) => {
+    let economy = parseFloat(
+      (runsAndBalls[obj].runs / runsAndBalls[obj].balls) * 6
+    );
+    runsAndBalls[obj].economy = economy;
+    delete runsAndBalls[obj].runs;
+    delete runsAndBalls[obj].balls;
+  });
+
+  return runsAndBalls;
+}
+
+//sorting find top 10 blower
+function findBowlerWithBestEconomy(economy) {
+  let sortedEconomy = Object.keys(economy).sort(
+    (a, b) => economy[a].economy - economy[b].economy
+  );
+  sortedEconomy = sortedEconomy.slice(0, 1).map((key) => ({
+    player: key,
+    economy: economy[key].economy,
+  }));
+  return sortedEconomy[0];
+}
+
+//finding top 10 economical blowers of 2015
+function findBestEconomicalBlowers(deliveries) {
+  const runsAndBalls = findRunsAndBallsInSuperOver(deliveries);
+  const calculateEconomy = toCalculateEconomy(runsAndBalls);
+  const bowlerWithBestEconomy = findBowlerWithBestEconomy(calculateEconomy);
+  return bowlerWithBestEconomy;
+}
+
+const jsonData = require("../../deliveries.json");
+console.log(findBestEconomicalBlowers(jsonData));
